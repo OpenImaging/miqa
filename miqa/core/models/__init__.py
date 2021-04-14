02a17b5d579a32f78428f21c836593670786085f
Zach Mullen <zach.mullen@kitware.com>
2021-04-14 14:05:08 -0400

    Make scan to note one-to-many

--- miqa/core/models/__init__.py
+++ miqa/core/models/__init__.py
@@ -1,7 +1,8 @@
 from .experiment import Experiment
 from .image import Image
 from .scan import Scan
+from .scan_note import ScanNote
 from .session import Session
 from .site import Site
 
-__all__ = ['Experiment', 'Image', 'Scan', 'Session', 'Site']
+__all__ = ['Experiment', 'Image', 'Scan', 'ScanNote', 'Session', 'Site']
---
+++ miqa/core/models/scan_note.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django_extensions.db.models import CreationDateTimeField
+
+
+class ScanNote(models.Model):
+    class Meta:
+        ordering = ['created']
+
+    created = CreationDateTimeField()
+    note = models.TextField(max_length=3000)
+    scan = models.ForeignKey('Scan', on_delete=models.CASCADE)
