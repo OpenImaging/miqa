9fed1f838033ca6251d24ae9be179fd7f984c540
Daniel Chiquito <daniel.chiquito@kitware.com>
2021-04-28 14:13:21 -0400

    Add all ViewSets to urls.py

--- miqa/urls.py
+++ miqa/urls.py
@@ -6,10 +6,38 @@ from drf_yasg.views import get_schema_view
 from rest_framework import permissions
 from rest_framework_extensions.routers import ExtendedSimpleRouter
 
-from miqa.core.rest import ImageViewSet
+from miqa.core.rest import (
+    ExperimentViewSet,
+    ImageViewSet,
+    ScanViewSet,
+    SessionViewSet,
+    SiteViewSet,
+)
 
 router = ExtendedSimpleRouter(trailing_slash=False)
-router.register('images', ImageViewSet)
+session_router = router.register('sessions', SessionViewSet, basename='session')
+session_router.register(
+    'experiments',
+    ExperimentViewSet,
+    basename='experiment',
+    parents_query_lookups=['session__id'],
+).register(
+    'scans',
+    ScanViewSet,
+    basename='scan',
+    parents_query_lookups=['experiment__session__id', 'experiment__id'],
+).register(
+    'images',
+    ImageViewSet,
+    basename='image',
+    parents_query_lookups=['scan__experiment__session__id', 'scan__experiment__id', 'scan__id'],
+)
+session_router.register(
+    'sites',
+    SiteViewSet,
+    basename='site',
+    parents_query_lookups=['session__id'],
+)
 
 # OpenAPI generation
 schema_view = get_schema_view(
