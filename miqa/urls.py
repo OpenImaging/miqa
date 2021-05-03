5b5ff2632df72f2eb87895d418f9177b98da7222
Daniel Chiquito <daniel.chiquito@kitware.com>
2021-05-03 12:12:31 -0400

    Flatten API structure in urls.py

--- miqa/urls.py
+++ miqa/urls.py
@@ -3,35 +3,16 @@ from django.contrib import admin
 from django.urls import include, path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
-from rest_framework import permissions
-from rest_framework_extensions.routers import ExtendedSimpleRouter
+from rest_framework import permissions, routers
 
 from miqa.core.rest import ExperimentViewSet, ImageViewSet, ScanViewSet, SessionViewSet, SiteViewSet
 
-router = ExtendedSimpleRouter(trailing_slash=False)
-session_router = router.register('sessions', SessionViewSet, basename='session')
-session_router.register(
-    'experiments',
-    ExperimentViewSet,
-    basename='experiment',
-    parents_query_lookups=['session__id'],
-).register(
-    'scans',
-    ScanViewSet,
-    basename='scan',
-    parents_query_lookups=['experiment__session__id', 'experiment__id'],
-).register(
-    'images',
-    ImageViewSet,
-    basename='image',
-    parents_query_lookups=['scan__experiment__session__id', 'scan__experiment__id', 'scan__id'],
-)
-session_router.register(
-    'sites',
-    SiteViewSet,
-    basename='site',
-    parents_query_lookups=['session__id'],
-)
+router = routers.SimpleRouter(trailing_slash=False)
+router.register('sessions', SessionViewSet, basename='session')
+router.register('experiments', ExperimentViewSet)
+router.register('scans', ScanViewSet)
+router.register('images', ImageViewSet)
+router.register('sites', SiteViewSet)
 
 # OpenAPI generation
 schema_view = get_schema_view(
