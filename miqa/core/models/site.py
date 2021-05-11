739a531c77be1babe7d94ceb031606eadd5e77f6
Zach Mullen <zach.mullen@kitware.com>
2021-05-11 14:16:55 -0400

    Convert model PKs to UUIDs
    For highly relational data that we wish to bulk insert, being able
    to create our own PKs for FK references is a boon. Also it seems
    like auto-incrementing PKs are more and more frowned upon for various reasons.

--- miqa/core/models/site.py
+++ miqa/core/models/site.py
@@ -1,9 +1,12 @@
+from uuid import uuid4
+
 from django.contrib.auth.models import User
 from django.db import models
 from django_extensions.db.models import TimeStampedModel
 
 
 class Site(TimeStampedModel, models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     name = models.CharField(max_length=255, db_index=True, unique=True, blank=False)
     creator = models.ForeignKey(User, on_delete=models.PROTECT)
 
