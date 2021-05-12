bb809d3abdf3cecd5035ee0874209463f13a57c4
Zach Mullen <zach.mullen@kitware.com>
2021-05-12 19:54:48 -0400

    Load images and scans via bulk insert
    This should substantially reduce the time for imports

--- miqa/core/conversion/csv_to_json.py
+++ miqa/core/conversion/csv_to_json.py
@@ -1,14 +1,14 @@
 # flake8: noqa N806
 
-import argparse
 import csv
 import io
 import json
 import os
-import sys
+from typing import Set
 
 
 def find_common_prefix(p1, p2):
+    # TODO obliterate this function
     minStrLen = min(len(p1), len(p2))
     firstNotMatch = 0
     while firstNotMatch < minStrLen:
@@ -25,10 +25,10 @@ def find_common_prefix(p1, p2):
     return p1[:firstNotMatch]
 
 
-def csvContentToJsonObject(csvContent):
+def csvContentToJsonObject(csvContent: str) -> dict:
     csvReader = csv.DictReader(io.StringIO(csvContent))
 
-    print('field names: {0}'.format(csvReader.fieldnames))
+    print('field names: {0}'.format(csvReader.fieldnames))  # TODO remove
 
     # First just blindly put everything in
     scans_raw = []
@@ -41,13 +41,13 @@ def csvContentToJsonObject(csvContent):
     # Gather commonalities
     common_path_prefix = scans_raw[0]['nifti_folder']
     experiment_dict = {}
-    site_set = set()
+    site_set: Set[str] = set()
     for scan in scans_raw:
         common_path_prefix = find_common_prefix(common_path_prefix, scan['nifti_folder'])
         scan['scan_path'] = '_'.join([scan['scan_id'], scan['scan_type']])
         experiment_dict[scan['xnat_experiment_id']] = scan['experiment_note']
 
-    print('common path prefix: {0}'.format(common_path_prefix))
+    print('common path prefix: {0}'.format(common_path_prefix))  # TODO remove
 
     # Produce final scans
     scans = []
@@ -70,7 +70,7 @@ def csvContentToJsonObject(csvContent):
             'note': scan['scan_note'],
             'experiment_id': scan['xnat_experiment_id'],
             'path': os.path.join(subdir, scan['scan_path']),
-            'imagePattern': r"^image[\d]*\.nii\.gz$",
+            'image_pattern': r"^image[\d]*\.nii\.gz$",
             'site_id': site,
         }
         if 'decision' in scan:
@@ -87,14 +87,12 @@ def csvContentToJsonObject(csvContent):
             }
         )
 
-    # Build output JSON object
-    outputObject = {}
-    outputObject['data_root'] = common_path_prefix
-    outputObject['scans'] = scans
-    outputObject['experiments'] = experiments
-    outputObject['sites'] = [{'id': site} for site in site_set]
-
-    return outputObject
+    return {
+        'data_root': common_path_prefix,
+        'scans': scans,
+        'experiments': experiments,
+        'sites': [{'name': site} for site in site_set],
+    }
 
 
 def csvToJson(csvFilePath, jsonFilePath):
@@ -109,38 +107,3 @@ def csvToJson(csvFilePath, jsonFilePath):
 
     with open(jsonFilePath, 'w') as fd:
         json.dump(jsonObject, fd)
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Convert CSV file to JSON format')
-    parser.add_argument(
-        'inputfile', type=str, help='Absolute system path to CSV file to be converted'
-    )
-    parser.add_argument(
-        '--output',
-        type=str,
-        default=None,
-        help='Absolute systemn path where JSON output should be '
-        + 'written.  If not provided, the default is to write '
-        + 'the output to the directory where the input file '
-        + 'is located and replace the "csv" extension with "json".',
-    )
-    args = parser.parse_args()
-
-    inputFilePath = args.inputfile
-
-    if not os.path.isfile(inputFilePath):
-        print(
-            'Please absolute provide path to input CSV file, {0} is not valid'.format(inputFilePath)
-        )
-        sys.exit(1)
-
-    outputFilePath = args.output
-
-    if not outputFilePath:
-        filePath = os.path.dirname(inputFilePath)
-        fileName = os.path.basename(inputFilePath)
-        nameWithoutExtension = fileName[: fileName.rindex('.')]
-        outputFilePath = os.path.join(filePath, '{0}.json'.format(nameWithoutExtension))
-
-    csvToJson(inputFilePath, outputFilePath)
