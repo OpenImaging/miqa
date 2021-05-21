a5d4f0bca29b79d56114ff2526a832a08bfdabbb
Daniel Chiquito <daniel.chiquito@kitware.com>
2021-05-21 16:08:35 -0400

    Add ScanNote view

--- miqa/core/rest/__init__.py
+++ miqa/core/rest/__init__.py
@@ -1,5 +1,6 @@
 from .experiment import ExperimentViewSet
 from .image import ImageViewSet
+from .scan_note import ScanNoteViewSet
 from .scan import ScanViewSet
 from .session import SessionViewSet
 from .site import SiteViewSet
@@ -7,6 +8,7 @@ from .site import SiteViewSet
 __all__ = [
     'ExperimentViewSet',
     'ImageViewSet',
+    'ScanNoteViewSet',
     'ScanViewSet',
     'SessionViewSet',
     'SiteViewSet',
---
+++ miqa/core/rest/scan_note.py
@@ -0,0 +1,57 @@
+from django.contrib.auth.models import User
+from django_filters import rest_framework as filters
+from rest_framework import serializers
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.viewsets import ModelViewSet
+
+from miqa.core.models import ScanNote
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name']
+
+
+class ScanNoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ScanNote
+        fields = ['id', 'creator', 'note', 'created', 'modified']
+
+    # Override the default DateTimeFields to disable read_only=True
+    created = serializers.DateTimeField()
+    modified = serializers.DateTimeField()
+
+    creator = UserSerializer()
+
+
+class CreateScanNoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ScanNote
+        fields = ['scan', 'note']
+
+
+class UpdateScanNoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ScanNote
+        fields = ['note']
+
+
+class ScanNoteViewSet(ModelViewSet):
+    queryset = ScanNote.objects.all()
+
+    filter_backends = [filters.DjangoFilterBackend]
+    filterset_fields = ['scan', 'creator']
+
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return CreateScanNoteSerializer
+        if self.request.method in ('PUT', 'PATCH'):
+            return UpdateScanNoteSerializer
+        return ScanNoteSerializer
+
+    def perform_create(self, serializer: CreateScanNoteSerializer):
+        note = ScanNote(**serializer.validated_data, creator=self.request.user)
+        note.save()
