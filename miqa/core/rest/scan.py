96393025c467a29cd0296f640d9aa113dbb10074
Daniel Chiquito <daniel.chiquito@kitware.com>
2021-05-21 16:08:35 -0400

    Add ScanNote view

--- miqa/core/rest/scan.py
+++ miqa/core/rest/scan.py
@@ -3,14 +3,17 @@ from rest_framework import serializers
 from rest_framework.permissions import AllowAny
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
-from miqa.core.models import Scan
+from miqa.core.models import Scan, ScanNote
+
+from .scan_note import ScanNoteSerializer
 
 
 class ScanSerializer(serializers.ModelSerializer):
     class Meta:
         model = Scan
-        fields = ['id', 'scan_id', 'scan_type', 'note', 'decision', 'experiment', 'site']
+        fields = ['id', 'scan_id', 'scan_type', 'notes', 'decision', 'experiment', 'site']
 
+    notes = ScanNoteSerializer(many=True)
     decision = serializers.ChoiceField(choices=Scan.decision.field.choices)
 
 
