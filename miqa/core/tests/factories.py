4e686c93dd43d76530c588e09b8de12331dd483c
Abhishek Venkat <abhisaivenkat@gmail.com>
2021-05-26 12:39:00 -0400

    fixed comparisons + added more tests

--- miqa/core/tests/factories.py
+++ miqa/core/tests/factories.py
@@ -1,14 +1,15 @@
 from django.contrib.auth.models import User
 import factory
+from datetime import datetime
 
 from miqa.core.models import (
     Session,
     Experiment,
     Scan,
+    ScanNote,
     Image,
     Site
 )
-
 
 class UserFactory(factory.django.DjangoModelFactory):
     class Meta:
@@ -48,7 +49,7 @@ class ExperimentFactory(factory.django.DjangoModelFactory):
 
     id = factory.Faker('uuid4')
     name = factory.Faker('safe_email')
-    note = factory.Faker('random_letters')
+    note = factory.Faker('pystr')
 
     session = factory.SubFactory(SessionFactory)
 
@@ -57,12 +58,34 @@ class ScanFactory(factory.django.DjangoModelFactory):
         model = Scan
 
     id = factory.Faker('uuid4')
-    name = factory.Faker('safe_email')
-    note = factory.Faker('random_letters')
 
     experiment = factory.SubFactory(ExperimentFactory)
     site = factory.SubFactory(SiteFactory)
 
-    scan_id = factory.Faker('random_letters')
-    scan_type = factory.Faker('random_letters')
+    scan_id = factory.Faker('pystr')
+    scan_type = factory.Faker('pystr')
     decision = 'Good'
+
+class NoteFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = ScanNote
+
+    id = factory.Faker('uuid4')
+    note = factory.Faker('pystr')
+    created = datetime.now()
+    modified = datetime.now()
+
+
+    creator = factory.SubFactory(UserFactory)
+    scan = factory.SubFactory(ScanFactory)
+
+
+class ImageFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Image
+
+    id = factory.Faker('uuid4')
+    raw_path = factory.Faker('pystr')
+    name = factory.Faker('safe_email')
+
+    scan = factory.SubFactory(ScanFactory)
