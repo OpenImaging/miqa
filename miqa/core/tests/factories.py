ed9efce9be8c115e72d7cd27952095d072100e88
Abhishek Venkat <abhisaivenkat@gmail.com>
2021-05-26 09:59:41 -0400

    django rest tests

--- miqa/core/tests/factories.py
+++ miqa/core/tests/factories.py
@@ -1,5 +1,13 @@
 from django.contrib.auth.models import User
-import factory.django
+import factory
+
+from miqa.core.models import (
+    Session,
+    Experiment,
+    Scan,
+    Image,
+    Site
+)
 
 
 class UserFactory(factory.django.DjangoModelFactory):
@@ -10,3 +18,51 @@ class UserFactory(factory.django.DjangoModelFactory):
     email = factory.Faker('safe_email')
     first_name = factory.Faker('first_name')
     last_name = factory.Faker('last_name')
+
+class SiteFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Site
+
+    id = factory.Faker('uuid4')
+    name = factory.Faker('safe_email')
+
+    creator = factory.SubFactory(UserFactory)
+
+
+
+class SessionFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Session
+
+    id = factory.Faker('uuid4')
+    name = factory.Faker('safe_email')
+
+    creator = factory.SubFactory(UserFactory)
+
+    import_path = '/fake/path'
+    export_path = '/fake/path'
+
+class ExperimentFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Experiment
+
+    id = factory.Faker('uuid4')
+    name = factory.Faker('safe_email')
+    note = factory.Faker('random_letters')
+
+    session = factory.SubFactory(SessionFactory)
+
+class ScanFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Scan
+
+    id = factory.Faker('uuid4')
+    name = factory.Faker('safe_email')
+    note = factory.Faker('random_letters')
+
+    experiment = factory.SubFactory(ExperimentFactory)
+    site = factory.SubFactory(SiteFactory)
+
+    scan_id = factory.Faker('random_letters')
+    scan_type = factory.Faker('random_letters')
+    decision = 'Good'
