64108fab2fcce8d7189a85ed82bd9aded02e2797
Dave Mackey <dave@davemackey.net>
2021-06-28 14:30:29 -0400

    Actually fixed the linting error.

--- miqa/core/tasks.py
+++ miqa/core/tasks.py
@@ -17,7 +17,7 @@ def import_data(user, session):
             csv_content = fd.read()
             try:
                 json_content = csvContentToJsonObject(csv_content)
-                validate(json_content, schema)  #  TODO this should be an internal error
+                validate(json_content, schema)  # TODO this should be an internal error
             except (ValidationError, Exception) as e:
                 raise ValidationError({'error': f'Invalid CSV file: {str(e)}'})
     elif session.import_path.endswith('.json'):
@@ -25,10 +25,10 @@ def import_data(user, session):
             try:
                 json_content = json.load(json_file)
                 validate(json_content, schema)
-            except (ValidationError, Exception) as e:  #  TODO this should be an internal error
+            except (ValidationError, Exception) as e:  # TODO this should be an internal error
                 raise ValidationError({'error': f'Invalid JSON file: {str(e)}'})
     # else:
-        #  TODO: Raise an error
+    # TODO: Raise an error
 
     data_root = Path(json_content['data_root'])
 
