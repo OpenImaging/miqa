38ae7c343f3dda7f32f0f3923791efaa1fd97348
Zach Mullen <zach.mullen@kitware.com>
2021-07-22 15:06:38 -0400

    Enable and fix testing

--- miqa/core/tests/fuzzy.py
+++ miqa/core/tests/fuzzy.py
@@ -2,11 +2,8 @@ import re
 
 
 class Re:
-    def __init__(self, pattern):
-        if isinstance(pattern, type(re.compile(''))):
-            self.pattern = pattern
-        else:
-            self.pattern = re.compile(pattern)
+    def __init__(self, pattern: str):
+        self.pattern = re.compile(pattern)
 
     def __eq__(self, other):
         return self.pattern.fullmatch(other) is not None
@@ -18,5 +15,4 @@ class Re:
         return repr(self.pattern.pattern)
 
 
-TIMESTAMP_RE = Re(r'\d{4}-\d{2}-\d{2}T\d{2}\:\d{2}\:\d{2}\.\d{6}Z')
 PATH_RE = Re(r'\/.+')
