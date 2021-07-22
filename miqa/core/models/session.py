4722302d678e7c8d8cdaebee2edfbec3b84be3aa
Zach Mullen <zach.mullen@kitware.com>
2021-07-22 15:38:20 -0400

    Add session lock field and lock management endpoints

--- miqa/core/models/session.py
+++ miqa/core/models/session.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from uuid import uuid4
 
 from django.contrib.auth.models import User
@@ -9,9 +11,16 @@ class Session(TimeStampedModel, models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     name = models.CharField(max_length=255)
     creator = models.ForeignKey(User, on_delete=models.PROTECT)
+    lock_owner = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, default=None, related_name='session_locks'
+    )
 
     import_path = models.CharField(max_length=500)
     export_path = models.CharField(max_length=500)
 
     def __str__(self):
         return self.name
+
+    @property
+    def session(self) -> 'Session':
+        return self
--- miqa/core/tests/test_rest.py
+++ miqa/core/tests/test_rest.py
@@ -11,7 +11,7 @@ def test_sessions_list(authenticated_api_client, session):
         'count': 1,
         'next': None,
         'previous': None,
-        'results': [{'id': session.id, 'name': session.name}],
+        'results': [{'id': session.id, 'name': session.name, 'lock_owner': None}],
     }
 
 
@@ -71,3 +71,58 @@ def test_images_list(authenticated_api_client, image):
     resp = authenticated_api_client.get('/api/v1/images')
     assert resp.status_code == 200
     assert resp.data['count'] == 1
+
+
+@pytest.mark.django_db
+def test_session_lock_acquire_requires_auth(api_client, session):
+    resp = api_client.post(f'/api/v1/sessions/{session.id}/lock')
+    assert resp.status_code == 401
+
+
+@pytest.mark.django_db
+def test_session_lock_acquire(authenticated_api_client, session):
+    resp = authenticated_api_client.post(f'/api/v1/sessions/{session.id}/lock')
+    assert resp.status_code == 204
+
+
+@pytest.mark.django_db
+def test_session_lock_reacquire_ok(api_client, session_factory, user):
+    session = session_factory(lock_owner=user)
+    api_client.force_authenticate(user=user)
+    resp = api_client.post(f'/api/v1/sessions/{session.id}/lock')
+    assert resp.status_code == 204
+
+
+@pytest.mark.django_db
+def test_session_lock_denied(authenticated_api_client, session_factory, user_factory):
+    owner = user_factory()
+    session = session_factory(lock_owner=owner)
+    resp = authenticated_api_client.post(f'/api/v1/sessions/{session.id}/lock')
+    assert resp.status_code == 409
+
+    session.refresh_from_db()
+    assert session.lock_owner == owner
+
+
+@pytest.mark.django_db
+def test_session_lock_release(api_client, session_factory, user):
+    session = session_factory(lock_owner=user)
+    api_client.force_authenticate(user=user)
+    resp = api_client.delete(f'/api/v1/sessions/{session.id}/lock')
+    assert resp.status_code == 204
+
+    session.refresh_from_db()
+    assert session.lock_owner is None
+
+
+@pytest.mark.django_db
+def test_session_lock_only_owner_can_release(
+    authenticated_api_client, session_factory, user_factory
+):
+    owner = user_factory()
+    session = session_factory(lock_owner=owner)
+    resp = authenticated_api_client.delete(f'/api/v1/sessions/{session.id}/lock')
+    assert resp.status_code == 409
+
+    session.refresh_from_db()
+    assert session.lock_owner.id == owner.id
