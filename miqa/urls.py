e6ae6abe514f7c41b5332e09ceeccfaf741db6ef
Anne Haley <anne.haley@kitware.com>
2021-09-02 13:57:32 -0400

    Add default redirect url for development mode

--- miqa/urls.py
+++ miqa/urls.py
@@ -1,6 +1,9 @@
+import os
+
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions, routers
@@ -47,6 +50,11 @@ urlpatterns = [
     path('api/docs/redoc/', schema_view.with_ui('redoc'), name='docs-redoc'),
     path('api/docs/swagger/', schema_view.with_ui('swagger'), name='docs-swagger'),
 ]
+
+if os.environ['DJANGO_CONFIGURATION'] == 'DevelopmentConfiguration':
+    urlpatterns = [
+        path('', RedirectView.as_view(url='http://localhost:8081'), name='home-redirect')
+    ] + urlpatterns
 
 if settings.DEBUG:
     import debug_toolbar
