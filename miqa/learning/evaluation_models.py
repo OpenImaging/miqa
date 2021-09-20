fcb6f8a404bbdbd62b72cafafa76aaf2b95d9a42
Anne Haley <anne.haley@kitware.com>
2021-09-20 12:57:01 -0400

    Add Evaluation model
    Fix linting and Evaluation on_delete

--- miqa/learning/evaluation_models.py
+++ miqa/learning/evaluation_models.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractclassmethod
+from typing import List
 
 from uri import URI
 
@@ -6,8 +7,9 @@ from miqa.learning.nn_classifier import get_model
 
 
 class EvaluationModel(ABC):
-    def __init__(self, uri: URI):
+    def __init__(self, uri: URI, expected_outputs: List[str]):
         self.uri = URI(uri)
+        self.expected_outputs = expected_outputs
         super().__init__()
 
     @abstractclassmethod
@@ -21,6 +23,23 @@ class NNModel(EvaluationModel):
 
 
 available_evaluation_models = {
-    'MIQAT1-0': NNModel('miqaT1-val0.pth'),
-    'MIQAT2-0': NNModel('fake_uri'),
+    'MIQAT1-0': NNModel(
+        'miqaT1-val0.pth',
+        [
+            'overall_quality',
+            'signal_noise_ratio',
+            'contrast_noise_ratio',
+            'normal_variants',
+            'lesions',
+            'full_brain_coverage',
+            'misalignment',
+            'swap_wraparound',
+            'ghosting_motion',
+            'inhomogeneity',
+            'susceptibility_metal',
+            'flow_artifact',
+            'truncation_artifact',
+        ],
+    ),
+    'MIQAT2-0': NNModel('fake_uri', []),
 }
