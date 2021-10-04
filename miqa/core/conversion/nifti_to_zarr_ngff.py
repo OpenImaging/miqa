9c2eef14aa41aa5ccb0dd2ce69e8e39b9c9c7047
Matt McCormick <matt.mccormick@kitware.com>
2021-10-04 15:55:02 -0400

    Convert to zarr when an image is added to the database
    A celery task is used. `clean` is called when the `Image` model is added
    to the database.
    
    Co-authored-by: Anne Haley <anne.haley@kitware.com>

--- miqa/core/conversion/nifti_to_zarr_ngff.py
+++ miqa/core/conversion/nifti_to_zarr_ngff.py
@@ -1,14 +1,22 @@
-__all__ = ['nifti_to_zarr_ngff']
+__all__ = ['nifti_to_zarr_ngff', 'convert_to_store_path']
 
 from pathlib import Path
 from typing import Union
 
+from celery import shared_task
 import itk
 import spatial_image_multiscale
 import spatial_image_ngff
 import zarr
 
 
+def convert_to_store_path(nifti_file: Union[str, Path]) -> Path:
+    """Provide the Zarr store Path for a Nifti path."""
+    store_path = Path(str(nifti_file) + '.zarr')
+    return store_path
+
+
+@shared_task
 def nifti_to_zarr_ngff(nifti_file: Union[str, Path]) -> Path:
     """Convert the nifti file on disk to a Zarr NGFF store.
 
@@ -16,7 +24,7 @@ def nifti_to_zarr_ngff(nifti_file: Union[str, Path]) -> Path:
 
     If the store already exists, it will not be re-created.
     """
-    store_path = Path(str(nifti_file) + '.zarr')
+    store_path = convert_to_store_path(nifti_file)
     if store_path.exists():
         return store_path
     image = itk.imread(str(nifti_file))
