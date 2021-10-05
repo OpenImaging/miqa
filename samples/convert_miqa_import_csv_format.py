0d73202c40546e6f1b6ec01b51c30895fae61f55
Anne Haley <anne.haley@kitware.com>
2021-10-05 12:32:56 -0400

    Convert existing samples to new import csv format

---
+++ samples/convert_miqa_import_csv_format.py
@@ -0,0 +1,65 @@
+import pandas
+from pathlib import Path
+import sys
+
+for file_name in sys.argv[1:]:
+    if not Path(file_name).exists():
+        raise Exception(f"{file_name} does not exist.")
+    if not file_name.endswith(".csv"):
+        raise Exception(f"{file_name} is not a CSV file.")
+    df = pandas.read_csv(file_name)
+    expected_columns = [
+        "xnat_experiment_id",
+        "nifti_folder",
+        "scan_id",
+        "scan_type",
+        "experiment_note",
+        "decision",
+        "scan_note",
+    ]
+    if len(df.columns) != len(expected_columns) or any(df.columns != expected_columns):
+        raise Exception(
+            f"{file_name} is not congruent with the old"
+            f" MIQA import format. Expected columns {str(expected_columns)} but recieved {str(df.columns)}."
+        )
+
+    project_name = str(input("Enter a project name for these scans:\n"))
+    print()
+    scan_type = str(
+        input(
+            "Enter the type of these scans (should be one of T1, T2, FMRI, MRA, PD, DWI, DTI):\n"
+        )
+    )
+    print()
+    new_rows = []
+    for index, row in df.iterrows():
+        new_rows.append(
+            [
+                project_name,
+                row["xnat_experiment_id"],
+                row["scan_type"],
+                scan_type,
+                row["scan_id"],
+                str(
+                    Path(
+                        row["nifti_folder"],
+                        str(row["scan_id"]) + "_" + row["scan_type"],
+                        "image.nii.gz",
+                    )
+                ),
+            ]
+        )
+    new_df = pandas.DataFrame(
+        new_rows,
+        columns=[
+            "project_id",
+            "experiment_id",
+            "scan_id",
+            "scan_type",
+            "frame_number",
+            "file_location",
+        ],
+    )
+    new_filename = "new_" + file_name.replace("old_", "")
+    new_df.to_csv(new_filename, index=False)
+    print("Wrote converted CSV to " + new_filename + ".")
