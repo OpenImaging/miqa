4b45fedb684c274373ead9aee87fd1040e0d57a5
Anne Haley <anne.haley@kitware.com>
2021-10-11 10:45:12 -0400

    Lint fixes

--- miqa/core/migrations/0015_scan_image_relationship.py
+++ miqa/core/migrations/0015_scan_image_relationship.py
@@ -47,10 +47,32 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='scan',
             name='scan_type',
-            field=models.CharField(choices=[('T1', 'T1'), ('T2', 'T2'), ('FMRI', 'FMRI'), ('MRA', 'MRA'), ('PD', 'PD'), ('DTI', 'DTI'), ('DWI', 'DWI'), ('ncanda-t1spgr-v1', 'ncanda-t1spgr-v1'), ('ncanda-mprage-v1', 'ncanda-mprage-v1'), ('ncanda-t2fse-v1', 'ncanda-t2fse-v1'), ('ncanda-dti6b500pepolar-v1', 'ncanda-dti6b500pepolar-v1'), ('ncanda-dti30b400-v1', 'ncanda-dti30b400-v1'), ('ncanda-dti60b1000-v1', 'ncanda-dti60b1000-v1'), ('ncanda-grefieldmap-v1', 'ncanda-grefieldmap-v1'), ('ncanda-rsfmri-v1', 'ncanda-rsfmri-v1')], default='T1', max_length=25),
+            field=models.CharField(
+                choices=[
+                    ('T1', 'T1'),
+                    ('T2', 'T2'),
+                    ('FMRI', 'FMRI'),
+                    ('MRA', 'MRA'),
+                    ('PD', 'PD'),
+                    ('DTI', 'DTI'),
+                    ('DWI', 'DWI'),
+                    ('ncanda-t1spgr-v1', 'ncanda-t1spgr-v1'),
+                    ('ncanda-mprage-v1', 'ncanda-mprage-v1'),
+                    ('ncanda-t2fse-v1', 'ncanda-t2fse-v1'),
+                    ('ncanda-dti6b500pepolar-v1', 'ncanda-dti6b500pepolar-v1'),
+                    ('ncanda-dti30b400-v1', 'ncanda-dti30b400-v1'),
+                    ('ncanda-dti60b1000-v1', 'ncanda-dti60b1000-v1'),
+                    ('ncanda-grefieldmap-v1', 'ncanda-grefieldmap-v1'),
+                    ('ncanda-rsfmri-v1', 'ncanda-rsfmri-v1'),
+                ],
+                default='T1',
+                max_length=25,
+            ),
         ),
         migrations.AddIndex(
             model_name='image',
-            index=models.Index(fields=['scan', 'frame_number'], name='core_image_scan_id_26e3ff_idx'),
+            index=models.Index(
+                fields=['scan', 'frame_number'], name='core_image_scan_id_26e3ff_idx'
+            ),
         ),
     ]
