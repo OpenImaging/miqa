778e898094927f4fe12998223476c7ced8a45435
Anne Haley <anne.haley@kitware.com>
2021-10-12 10:03:10 -0400

    Restructure import conversion, add support for JSON imports, add support for exports

--- miqa/core/tasks.py
+++ miqa/core/tasks.py
@@ -2,12 +2,14 @@ import json
 from pathlib import Path
 
 from celery import shared_task
-from jsonschema.exceptions import ValidationError
 import pandas
 
-from miqa.core.conversion.csv_to_json import find_common_prefix
-from miqa.core.conversion.json_to_csv import jsonObjectToCsvContent
-from miqa.core.models import Decision, Evaluation, Experiment, Image, Project, Scan
+from miqa.core.conversion.import_export_csvs import (
+    IMPORT_CSV_COLUMNS,
+    import_dataframe_to_dict,
+    validate_import_dict,
+)
+from miqa.core.models import Evaluation, Experiment, Image, Project, Scan
 from miqa.learning.evaluation_models import available_evaluation_models
 from miqa.learning.nn_inference import evaluate_many
 
@@ -44,169 +46,88 @@ def evaluate_data(image_ids, project_id):
 
 @shared_task
 def import_data(user_id, project_id):
-    # user = User.objects.get(id=user_id)
     project = Project.objects.get(id=project_id)
+
+    if project.import_path.endswith('.csv'):
+        import_dict = import_dataframe_to_dict(pandas.read_csv(project.import_path))
+    elif project.import_path.endswith('.json'):
+        import_dict = json.load(open(project.import_path))
+    else:
+        raise ValueError(f'Invalid import file {project.import_path}.')
+
+    if not validate_import_dict(import_dict):
+        raise ValueError(f'Invalid format of import file {project.import_path}')
 
     new_projects = []
     new_experiments = []
     new_scans = []
     new_frames = []
 
-    if project.import_path.endswith('.csv'):
-        df = pandas.read_csv(project.import_path)
-        expected_columns = [
-            'project_name',
-            'experiment_name',
-            'scan_name',
-            'scan_type',
-            'frame_number',
-            'file_location',
-        ]
-        if list(df.columns) != expected_columns:
-            raise ValidationError(
-                f'Import file has invalid columns. Expected {str(expected_columns)}'
-            )
+    for _project_name, project_data in import_dict['projects'].items():
+        # Switch these lines to support multi-project imports
+        # project_object = Project(name=_project_name)
+        # new_projects.append(project_object)
+        project_object = project
 
-        # TODO: put this back for support of multiple projects in one import
-        # these_projects = {
-        #     project_name: Project(name=project_name, creator=user)
-        #     for project_name in df['project_name'].unique()
-        # })
-        # new_projects.extend([project for _name, project in these_projects.items()])
-        # for project_name, project_object in new_projects[0].items():
-        for project_name, project_object in [(df['project_name'].mode()[0], project)]:
+        # delete old imports of these projects
+        Experiment.objects.filter(
+            project=project_object
+        ).delete()  # cascades to scans -> images, scan_notes
 
-            # delete old imports of these projects
-            Experiment.objects.filter(
-                project=project_object
-            ).delete()  # cascades to scans -> images, scan_notes
+        for experiment_name, experiment_data in project_data['experiments'].items():
+            experiment_object = Experiment(name=experiment_name, project=project_object)
+            new_experiments.append(experiment_object)
 
-            these_experiments = {
-                experiment_name: Experiment(name=experiment_name, project=project_object)
-                for experiment_name in df[df['project_name'] == project_name][
-                    'experiment_name'
-                ].unique()
-            }
-            new_experiments.extend([experiment for _name, experiment in these_experiments.items()])
-            for experiment_name, experiment_object in these_experiments.items():
-                these_scans = {
-                    scan_name: Scan(
-                        name=scan_name, scan_type=scan_type, experiment=experiment_object
+            for scan_name, scan_data in experiment_data['scans'].items():
+                scan_object = Scan(
+                    name=scan_name, scan_type=scan_data['type'], experiment=experiment_object
+                )
+                new_scans.append(scan_object)
+                for frame_number, frame_data in scan_data['frames'].items():
+                    raw_path = frame_data['file_location']
+                    if raw_path[0] != '/':
+                        # not an absolute file path; refer to project import csv location
+                        raw_path = str(Path(project.import_path).parent.parent / raw_path)
+                        # TODO: add support for interpreting URIs not on host machine
+                    if not Path(raw_path).exists():
+                        raise ValueError(f'Could not locate file "{raw_path}".')
+
+                    frame_object = Image(
+                        frame_number=frame_number,
+                        raw_path=raw_path,
+                        scan=scan_object,
                     )
-                    for scan_name, scan_type in df[
-                        (df['experiment_name'] == experiment_name)
-                        & (df['project_name'] == project_name)
-                    ]
-                    .groupby(['scan_name', 'scan_type'])
-                    .groups
-                }
-                new_scans.extend([scan for _name, scan in these_scans.items()])
-                for scan_name, scan_object in these_scans.items():
-                    these_frames = {}
-                    for row in df[
-                        (df['scan_name'] == scan_name)
-                        & (df['experiment_name'] == experiment_name)
-                        & (df['project_name'] == project_name)
-                    ].iterrows():
-                        raw_path = row[1]['file_location']
-                        if raw_path[0] != '/':
-                            # not an absolute file path; refer to project import csv location
-                            raw_path = str(Path(project.import_path).parent.parent / raw_path)
-                            # TODO: add support for interpreting URIs not on host machine
-                        if not Path(raw_path).exists():
-                            raise ValidationError(f'Could not locate file "{raw_path}".')
-                        these_frames[row[1]['frame_number']] = Image(
-                            frame_number=int(row[1]['frame_number']),
-                            scan=scan_object,
-                            raw_path=raw_path,
-                        )
-                    new_frames.extend([frame for _name, frame in these_frames.items()])
+                    new_frames.append(frame_object)
 
-        Project.objects.bulk_create(new_projects)
-        Experiment.objects.bulk_create(new_experiments)
-        Scan.objects.bulk_create(new_scans)
-        Image.objects.bulk_create(new_frames)
+    Project.objects.bulk_create(new_projects)
+    Experiment.objects.bulk_create(new_experiments)
+    Scan.objects.bulk_create(new_scans)
+    Image.objects.bulk_create(new_frames)
 
-        evaluate_data.delay([frame.id for frame in new_frames], project.id)
-
-    # TODO: re-write JSON parsing to be generalized like CSV parsing
-    # elif project.import_path.endswith('.json'):
-    #     pass
-    else:
-        raise ValidationError(f'Invalid import file {project.import_path}.')
+    evaluate_data.delay([frame.id for frame in new_frames], project.id)
 
 
 @shared_task
 def export_data(project_id):
-    project = Project.objects.get(id=project_id)
+    project_object = Project.objects.get(id=project_id)
+    parent_location = Path(project_object.export_path).parent
+    if not parent_location.exists():
+        raise ValueError(f'No such location {parent_location} to create export file.')
 
-    data_root = None
-    experiments = []
-    scans = []
-    sites = set()
+    export_data = []
 
-    for experiment in project.experiments.all():
-        experiments.append(
-            {
-                'id': experiment.name,
-                'note': experiment.note,
-            }
-        )
-        for scan in experiment.scans.all():
-            scan_root = None
-            for image in scan.images.all():
-                if data_root is None:
-                    data_root = image.raw_path
-                data_root = find_common_prefix(data_root, image.raw_path)
-
-                if scan_root is None:
-                    scan_root = image.raw_path
-                scan_root = find_common_prefix(scan_root, image.raw_path)
-
-            if scan_root is None:
-                # There were no images in this scan, don't export it
-                continue
-
-            decision = scan.decisions.order_by('-created').first()
-            if decision:
-                decision = Decision.to_rating(decision.decision)
-            else:
-                decision = ''
-
-            note = '\n'.join(
-                [f'{note.initials}:{note.note}' for note in scan.notes.order_by('created').all()]
-            )
-
-            scans.append(
-                {
-                    'id': scan.scan_id,
-                    'type': scan.scan_type,
-                    'note': note,
-                    'experiment_id': experiment.name,
-                    'path': scan_root,
-                    'image_pattern': r'^image[\d]*\.nii\.gz$',
-                    'site_id': scan.site.name,
-                    'decision': decision,
-                }
-            )
-            sites.add(scan.site.name)
-
-    # scan.path should omit the data_root prefix, so trim it off now
-    for scan in scans:
-        scan['path'] = scan['path'][len(data_root) :]
-
-    json_content = {
-        'data_root': data_root,
-        'experiments': experiments,
-        'scans': scans,
-        'sites': [{'name': site} for site in sites],
-    }
-
-    if project.export_path.endswith('.csv'):
-        with open(project.export_path, 'w') as csv_file:
-            csv_content = jsonObjectToCsvContent(json_content)
-            csv_file.write(csv_content.getvalue())
-    else:
-        # Assume JSON
-        with open(project.export_path, 'w') as json_file:
-            json_file.write(json.dumps(json_content, indent=4))
+    for experiment_object in Experiment.objects.filter(project=project_object):
+        for scan_object in Scan.objects.filter(experiment=experiment_object):
+            for frame_object in Image.objects.filter(scan=scan_object):
+                export_data.append(
+                    [
+                        project_object.name,
+                        experiment_object.name,
+                        scan_object.name,
+                        scan_object.scan_type,
+                        frame_object.frame_number,
+                        frame_object.raw_path,
+                    ]
+                )
+    export_df = pandas.DataFrame(export_data, columns=IMPORT_CSV_COLUMNS)
+    export_df.to_csv(project_object.export_path, index=False)
