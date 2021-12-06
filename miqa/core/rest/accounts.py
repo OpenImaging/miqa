b4682f16dda082f09179d7f233ebfa5a295e9a4a
Anne Haley <anne.haley@kitware.com>
2021-12-06 11:50:25 -0500

    Lint fix

--- miqa/core/rest/accounts.py
+++ miqa/core/rest/accounts.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.contrib.auth import logout
 from django.contrib.auth.models import User
 from django.core.mail import EmailMultiAlternatives
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect, render
 from django.views.generic.base import TemplateView
 from rest_framework import status
 from rest_framework.response import Response
