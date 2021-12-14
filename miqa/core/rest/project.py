f8a69a8437a14f6e98d0ce222d8932e6b64fa5ba
Anne Haley <anne.haley@kitware.com>
2021-12-14 12:16:48 -0500

    Add task-overview endpoint for Projects

--- miqa/core/rest/project.py
+++ miqa/core/rest/project.py
@@ -1,5 +1,5 @@
 from drf_yasg.utils import no_body, swagger_auto_schema
-from guardian.shortcuts import get_objects_for_user, get_users_with_perms
+from guardian.shortcuts import get_objects_for_user, get_users_with_perms, get_perms
 from rest_framework import serializers, status, mixins
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
@@ -41,6 +41,52 @@ class ProjectSettingsSerializer(serializers.ModelSerializer):
         ]
 
         return permissions
+
+
+class ProjectTaskOverviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = ['total_experiments', 'total_scans', 'my_project_role', 'scan_states']
+
+    total_experiments = serializers.SerializerMethodField('get_total_experiments')
+    total_scans = serializers.SerializerMethodField('get_total_scans')
+    my_project_role = serializers.SerializerMethodField('get_my_project_role')
+    scan_states = serializers.SerializerMethodField('get_scan_states')
+
+    def get_total_experiments(self, obj):
+        return obj.experiments.count()
+
+    def get_total_scans(self, obj):
+        return sum([exp.scans.count() for exp in obj.experiments.all()])
+
+    def get_highest_perm(self, user, project):
+        perm_order = Project.get_read_permission_groups()
+        return sorted(
+            get_perms(user, project),
+            key=lambda perm: perm_order.index(perm) if perm in perm_order else -1,
+        )[-1]
+
+    def get_my_project_role(self, obj):
+        return self.get_highest_perm(self.context['user'], obj)
+
+    def get_scan_states(self, obj):
+        def convert_state_string(last_reviewer_role):
+            if last_reviewer_role == 'tier_2_reviewer':
+                return 'complete'
+            elif last_reviewer_role == 'tier_1_reviewer':
+                return 'needs tier 2 review'
+            else:
+                return last_reviewer_role
+
+        return {
+            str(scan.id): convert_state_string(
+                self.get_highest_perm(scan.decisions.latest('created').creator, obj)
+                if scan.decisions.count() > 0
+                else 'unreviewed'
+            )
+            for exp in obj.experiments.all()
+            for scan in exp.scans.all()
+        }
 
 
 class ProjectRetrieveSerializer(serializers.ModelSerializer):
@@ -164,3 +210,16 @@ class ProjectViewSet(
         export_data(project.id)
 
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+    @swagger_auto_schema(
+        request_body=no_body,
+        responses={200: ProjectTaskOverviewSerializer()},
+    )
+    @project_permission_required()
+    @action(detail=True, methods=['GET'])
+    def task_overview(self, request, **kwargs):
+        project: Project = self.get_object()
+        return Response(
+            ProjectTaskOverviewSerializer(project, context={'user': request.user}).data,
+            status=status.HTTP_200_OK,
+        )
