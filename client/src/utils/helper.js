df9a5bbf90710a86566a54d81976d81272e09cc2
Zach Mullen <zach.mullen@kitware.com>
2022-01-14 10:10:35 -0500

    Improved progress rendering on first frame download

--- client/src/utils/helper.js
+++ client/src/utils/helper.js
@@ -1,8 +1,21 @@
-// eslint-disable-next-line import/prefer-default-export
 export function cleanFrameName(name) {
   const cleanName = name.replace(/^image/, '').replace(/.nii.gz$/, '');
   if (cleanName === '') {
     return '1';
   }
   return cleanName;
+}
+
+export function formatSize(size, { base = 1024, unit = 'B' } = {}) {
+  if (size < base) {
+    return `${size} ${unit}`;
+  }
+
+  let i;
+  let val = size;
+  for (i = 0; val >= base && i < 4; i += 1) {
+    val /= base;
+  }
+
+  return `${val.toFixed(2)}  ${['', 'K', 'M', 'G', 'T'][i]}${unit}`;
 }
