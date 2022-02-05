f97637fef9ff91c0cb983169c90c42d5de697eb4
Anne Haley <anne.haley@kitware.com>
2022-02-04 22:03:54 -0500

    Frame downloads use 'content' field

--- miqa/core/rest/experiment.py
+++ miqa/core/rest/experiment.py
@@ -3,14 +3,14 @@ from django.db import transaction
 from django_filters import rest_framework as filters
 from drf_yasg.utils import no_body, swagger_auto_schema
 from guardian.shortcuts import get_objects_for_user
-from rest_framework import serializers, status
+from rest_framework import mixins, serializers, status
 from rest_framework.decorators import action
 from rest_framework.fields import UUIDField
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
-from miqa.core.models import Experiment, Project, ScanDecision
+from miqa.core.models import Experiment, Frame, Project, Scan, ScanDecision
 from miqa.core.rest.permissions import project_permission_required
 from miqa.core.rest.scan import ScanSerializer
 
@@ -43,11 +43,18 @@ class ExperimentSerializer(serializers.ModelSerializer):
     project = serializers.PrimaryKeyRelatedField(read_only=True, pk_field=UUIDField())
 
 
-class ExperimentViewSet(ReadOnlyModelViewSet):
-    # Our default serializer nests its experiment (not sure why though)
+class ExperimentCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Experiment
+        fields = ['name', 'project']
 
+    project = serializers.PrimaryKeyRelatedField(
+        queryset=Project.objects.all(), pk_field=UUIDField()
+    )
+
+
+class ExperimentViewSet(ReadOnlyModelViewSet, mixins.CreateModelMixin, mixins.UpdateModelMixin):
     filter_backends = [filters.DjangoFilterBackend]
-    permission_classes = [IsAuthenticated, UserHoldsExperimentLock]
     serializer_class = ExperimentSerializer
 
     def get_queryset(self):
@@ -58,8 +65,47 @@ class ExperimentViewSet(ReadOnlyModelViewSet):
         )
         return Experiment.objects.filter(project__in=projects)
 
+    def get_permissions(self):
+        if self.request.method == 'DELETE':
+            return [IsAuthenticated(), UserHoldsExperimentLock()]
+        else:
+            return [IsAuthenticated()]
+
+    @swagger_auto_schema(
+        request_body=ExperimentCreateSerializer(),
+        responses={201: ExperimentSerializer},
+    )
+    def create(self, request, *args, **kwargs):
+        serializer = ExperimentCreateSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        experiment = Experiment(
+            project=Project.objects.get(id=serializer.data['project']),
+            name=serializer.data['name'],
+            lock_owner=None,
+        )
+        experiment.save()
+        return Response(
+            ExperimentSerializer(experiment).data,
+            status=status.HTTP_201_CREATED,
+        )
+
+    def update(self, request, *args, **kwargs):
+        experiment: Experiment = self.get_object()
+        if 'frames' in request.data:
+            num_existing_scans = experiment.scans.count()
+            for index, s3_reference in enumerate(request.data['frames']):
+                new_scan = Scan(
+                    name=f'SCAN_{index + num_existing_scans}',
+                    experiment=experiment,
+                )
+                new_scan.save()
+                new_frame = Frame(content=s3_reference, scan=new_scan)
+                new_frame.save()
+            del request.data['frames']
+        return super().update(request, *args, **kwargs)
+
     @project_permission_required(experiments__pk='pk')
-    @action(detail=True, methods=['POST'], permission_classes=[IsAuthenticated])
+    @action(detail=True, methods=['POST'])
     def note(self, request, pk=None):
         experiment_object = self.get_object()
         experiment_object.note = request.data['note']
@@ -77,7 +123,7 @@ class ExperimentViewSet(ReadOnlyModelViewSet):
         },
     )
     @project_permission_required(review_access=True, experiments__pk='pk')
-    @action(detail=True, methods=['POST'], permission_classes=[IsAuthenticated])
+    @action(detail=True, methods=['POST'])
     def lock(self, request, pk=None):
         """Acquire the exclusive write lock on this experiment."""
         with transaction.atomic():
