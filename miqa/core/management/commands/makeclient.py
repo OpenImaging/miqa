a92aa4d6c300edc36a300005294df6808dc9ed23
Daniel Chiquito <daniel.chiquito@kitware.com>
2022-02-07 10:03:21 -0500

    Make makeclient idempotent

--- miqa/core/management/commands/makeclient.py
+++ miqa/core/management/commands/makeclient.py
@@ -2,24 +2,31 @@ from django.contrib.auth.models import User
 import djclick as click
 from oauth2_provider.models import Application
 
+CLIENT_ID = 'cBmD6D6F2YAmMWHNQZFPUr4OpaXVpW5w4Thod6Kj'
+
 
 # create django oauth toolkit appliction (client)
-@click.option('--username', type=click.STRING, help='superuser username for application creator')
-@click.option('--uri', type=click.STRING, help='redirect uri for application')
+@click.option(
+    '--username',
+    type=click.STRING,
+    required=True,
+    help='superuser username for application creator',
+)
+@click.option('--uri', type=click.STRING, required=True, help='redirect uri for application')
 @click.command()
 def command(username, uri):
+    if Application.objects.filter(client_id=CLIENT_ID).exists():
+        click.echo('The client already exists. You can administer it from the admin console.')
+        return
 
     if username:
-
         user = User.objects.get(username=username)
-
     else:
-
         user = User.objects.first()
 
     application = Application(
         name='miqa-client',
-        client_id='cBmD6D6F2YAmMWHNQZFPUr4OpaXVpW5w4Thod6Kj',
+        client_id=CLIENT_ID,
         client_secret='',
         client_type='public',
         redirect_uris=uri,
@@ -27,5 +34,4 @@ def command(username, uri):
         user_id=user.id,
         skip_authorization=True,
     )
-
     application.save()
