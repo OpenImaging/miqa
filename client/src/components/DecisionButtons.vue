11c342531f0ec707a693c7559299b038396bc552
Anne Haley <anne.haley@kitware.com>
2022-03-24 11:16:45 -0400

    Fix keycapture bug

--- client/src/components/DecisionButtons.vue
+++ client/src/components/DecisionButtons.vue
@@ -135,7 +135,10 @@ export default {
       p: 'UN',
     };
     window.addEventListener('keydown', (event) => {
-      if (Object.keys(decisionShortcuts).includes(event.key)) {
+      if (
+        (this.$refs.commentInput && !this.$refs.commentInput.isFocused)
+          && Object.keys(decisionShortcuts).includes(event.key)
+      ) {
         const code = decisionShortcuts[event.key];
         if (this.options.map(
           (option) => option.code,
@@ -396,6 +399,7 @@ export default {
         class="pt-5"
       >
         <v-textarea
+          ref="commentInput"
           v-model="newComment"
           :counter="!warnDecision"
           :hide-details="warnDecision"
