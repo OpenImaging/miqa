91219a3c2bda576bfa12cf1518e3735101164b92
Anne Haley <anne.haley@kitware.com>
2022-04-01 11:28:52 -0400

    Lint fix

--- miqa/core/rest/exceptions.py
+++ miqa/core/rest/exceptions.py
@@ -1,10 +1,10 @@
 import logging
 import uuid
+
 from rest_framework import status
 from rest_framework.exceptions import APIException
-from rest_framework.views import exception_handler
 from rest_framework.response import Response
-
+from rest_framework.views import exception_handler
 
 logger = logging.getLogger('MIQA')
 
