f37cd1cb9302e7cf417978e03f08a3fa25a74fc5
Brian Helba <brian.helba@kitware.com>
2022-05-04 20:07:24 -0400

    Allow the client to be deployed with Cloudflare Pages

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,8 +1,12 @@
+const fs = require('fs');
+const util = require('util');
 const webpack = require('webpack');
 const CopyPlugin = require('copy-webpack-plugin');
 const path = require('path');
 const vtkChainWebpack = require('vtk.js/Utilities/config/chainWebpack');
 const packageJson = require('./package.json');
+
+const stat = util.promisify(fs.stat);
 
 module.exports = {
   devServer: {
@@ -21,6 +25,33 @@ module.exports = {
             // invalid "main" field, which causes warnings on every build.
             from: path.join(__dirname, 'node_modules', 'itk'),
             to: 'itk',
+            filter: async (resourcePath) => {
+              const resourceStats = await stat(resourcePath);
+              const resourceSize = resourceStats.size;
+              // Exclude files larger than 25MB.
+              // Cloudflare Pages does not allow assets larger than this:
+              // https://developers.cloudflare.com/pages/platform/limits/#file-size
+              // In a typical library, module resolution is handled by the bundler (Webpack, Parcel,
+              // etc.), which can perform tree shaking to exclude unused content from the final
+              // build and can make choices as to how to split and potentially asynchronously load
+              // built modules. See: https://webpack.js.org/guides/code-splitting/
+              // However, itk.js instead internally implements its own runtime asynchronous module
+              // loader:
+              // https://github.com/InsightSoftwareConsortium/itk-wasm/blob/v14.0.0/src/loadEmscriptenModuleBrowser.js
+              // and expects the build system to copy the entire itk.js library to the output
+              // directory:
+              // https://github.com/InsightSoftwareConsortium/itk-wasm/blob/v14.0.0/doc/content/examples/webpack.md
+              // Even if the bundler performs tree-shaking of the actual pure-Javascript code paths
+              // through itk.js, it's impossible to know what other files might be internally
+              // fetched by itk.js dynamically at runtime.
+              // So, here we'll copy everything by default, but exclude the largest files to
+              // make this application deployable, and hope that none of the excluded files were
+              // actually necessary at runtime.
+              // With itk@14.0.0, only "itk/PolyDataIOs/VTKExodusFileReader.js" is actually
+              // excluded.
+              // The semantics of "CopyPlugin.filter" are the same as "Array.prototype.filter".
+              return resourceSize <= (25 * 1024 * 1024);
+            },
           },
         ],
       }),
