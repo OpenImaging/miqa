e113dc908ec6e56b49f23f264074b6043fa0a7f2
Anne Haley <anne.haley@kitware.com>
2022-05-18 12:03:20 -0400

    Add user_id argument to EmailAddress get_or_create

--- miqa/core/management/populate_demo.py
+++ miqa/core/management/populate_demo.py
@@ -21,7 +21,10 @@ def populate_demo():
         )
         demo_user.set_password('demoMe')
         demo_user.save()
-        demo_user_email = EmailAddress.objects.get_or_create(email='test@miqa.dev')
+        demo_user_email, created = EmailAddress.objects.get_or_create(
+            email='test@miqa.dev',
+            user_id=demo_user.id,
+        )
         demo_user_email.verified = True
         demo_user_email.save()
 
