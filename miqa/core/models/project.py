ba51aeb17481047426c09c5a11c71f487536c2e1
Anne Haley <anne.haley@kitware.com>
2022-05-31 12:20:12 -0400

    Use TextChoices in definition of `anatomy_orientation` field

--- miqa/core/models/project.py
+++ miqa/core/models/project.py
@@ -32,6 +32,11 @@ def default_evaluation_model_mapping():
     }
 
 
+class AnatomyOrientation(models.TextChoices):
+    LPS = 'LPS'
+    RAS = 'RAS'
+
+
 class Project(TimeStampedModel, models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     name = models.CharField(max_length=255)
@@ -40,8 +45,8 @@ class Project(TimeStampedModel, models.Model):
     import_path = models.CharField(max_length=500, blank=True)
     export_path = models.CharField(max_length=500, blank=True)
     anatomy_orientation = models.CharField(
-        choices=[('LPS', 'LPS'), ('RAS', 'RAS')],
-        default='LPS',
+        choices=AnatomyOrientation.choices,
+        default=AnatomyOrientation.LPS,
         max_length=3,
     )
     s3_public = models.BooleanField(
