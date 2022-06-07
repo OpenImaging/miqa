f815b7e0913e4538e568577f4f8bf62f8545dc51
Dženan Zukić <dzenan.zukic@kitware.com>
2022-06-07 11:16:48 +0200

    Make overallQA loss dominant again

--- miqa/learning/nn_training.py
+++ miqa/learning/nn_training.py
@@ -212,7 +212,7 @@ class CombinedLoss(torch.nn.Module):
         # if we make overall QA a lot more important than individual artifacts,
         # then accuracy of the artifact predictions is very low
         # overallQA has 0-10, individual artifacts 0-1 range
-        loss = 0.1 * qa_loss
+        loss = 10 * qa_loss
 
         for i in range(self.presence_count):
             i_target = target[..., i + regression_count]
