9ad531d60498d42b7330a3cfc5384f7d12aa6b9f
Anne Haley <anne.haley@kitware.com>
2022-07-11 10:04:55 -0400

    Convert classes with simple constructors to dataclasses

--- python_client/decision.py
+++ python_client/decision.py
@@ -1,3 +1,12 @@
+from typing import TYPE_CHECKING
+from dataclasses import dataclass
+
+
+if TYPE_CHECKING:
+    from scan import Scan
+
+
+@dataclass
 class ScanDecision:
     """
     Attributes:
@@ -7,26 +16,14 @@ class ScanDecision:
 
     """
 
-    def __init__(
-        self,
-        id: str,
-        decision: str,
-        scan,
-        creator: dict,
-        created: str,
-        note: str,
-        user_identified_artifacts: dict,
-        location: dict,
-        **kwargs,
-    ):
-        self.id = id
-        self.decision_code = decision
-        self.scan = scan
-        self.creator = creator
-        self.creation_time = created
-        self.note = note
-        self.user_identified_artifacts = user_identified_artifacts
-        self.location = location
+    id: str
+    decision: str
+    scan: "Scan"
+    creator: dict
+    created: str
+    note: str
+    user_identified_artifacts: dict
+    location: dict
 
     def __repr__(self):
         return f"Scan Decision {self.decision_code}"
--- python_client/frame.py
+++ python_client/frame.py
@@ -1,28 +1,27 @@
+from typing import TYPE_CHECKING
+from dataclasses import dataclass
+
+
+if TYPE_CHECKING:
+    from scan import Scan
+
+
+@dataclass
 class Frame:
     """
     Attributes:
-      id, number, evaluation,
-      file_extension, download_url
+      id, frame_number, frame_evaluation,
+      extension, download_url
     Functions:
 
     """
 
-    def __init__(
-        self,
-        id: str,
-        frame_number: int,
-        scan,
-        frame_evaluation: dict,
-        extension: str,
-        download_url: str,
-        **kwargs,
-    ):
-        self.id = id
-        self.number = frame_number
-        self.scan = scan
-        self.evaluation = frame_evaluation
-        self.file_extension = extension
-        self.download_url = download_url
+    id: str
+    frame_number: int
+    scan: "Scan"
+    frame_evaluation: dict
+    extension: str
+    download_url: str
 
     def __repr__(self):
         return f"Frame {self.number}"
