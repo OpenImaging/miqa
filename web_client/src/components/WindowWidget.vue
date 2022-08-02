e80869ab8993d8fb7e60a5e0e98d906d4435b08c
Anne Haley <anne.haley@kitware.com>
2022-08-02 13:37:44 -0400

    Use ww and wl on one range slider

--- web_client/src/components/WindowWidget.vue
+++ web_client/src/components/WindowWidget.vue
@@ -18,48 +18,45 @@ export default defineComponent({
     },
   },
   setup(props) {
-    const selectedPreset = ref();
-    const windowLocked = computed(() => store.state.windowLocked);
+    const currentRange = ref();
     const currentWindowWidth = computed(() => store.state.currentWindowWidth);
     const currentWindowLevel = computed(() => store.state.currentWindowLevel);
     const widthMin = computed(() => (props.representation && props.representation.getPropertyDomainByName('windowWidth').min) || 0);
     const widthMax = computed(() => (props.representation && Math.ceil(props.representation.getPropertyDomainByName('windowWidth').max)) || 0);
-    const levelMin = computed(() => (props.representation && props.representation.getPropertyDomainByName('windowLevel').min) || 0);
-    const levelMax = computed(() => (props.representation && Math.ceil(props.representation.getPropertyDomainByName('windowLevel').max)) || 0);
+    const selectedPreset = ref();
+    const windowLocked = computed(() => store.state.windowLocked);
     const setWindowLocked = (lock) => store.commit.setWindowLocked(lock);
 
-    function updateWindow(width: number, level: number) {
-      props.representation.setWindowWidth(width);
-      props.representation.setWindowLevel(level);
+    const wholeRange = widthMax.value - widthMin.value;
+    currentRange.value = [
+      widthMin.value + wholeRange * 0.2,
+      widthMax.value - wholeRange * 0.2,
+    ];
+
+    function updateRender([v0, v1]) {
+      const ww = v1 - v0;
+      const wl = v0 + Math.ceil(ww / 2);
+      props.representation.setWindowWidth(ww);
+      props.representation.setWindowLevel(wl);
     }
+
+    watch(currentRange, updateRender);
+
     function applyPreset(preset) {
-      updateWindow(preset.width, preset.level);
+      console.log('apply preset', preset, 'to range');
+      // updateWindow(preset.width, preset.level);
       setWindowLocked(true);
     }
 
-    watch(currentWindowWidth, (value) => {
-      if (Number.isInteger(value)) {
-        props.representation.setWindowWidth(value);
-      }
-    });
-
-    watch(currentWindowLevel, (value) => {
-      if (Number.isInteger(value)) {
-        props.representation.setWindowLevel(value);
-      }
-    });
-
     return {
-      selectedPreset,
-      windowLocked,
+      currentRange,
       currentWindowWidth,
       currentWindowLevel,
+      selectedPreset,
+      windowLocked,
       setWindowLocked,
       widthMin,
       widthMax,
-      levelMin,
-      levelMax,
-      updateWindow,
       windowPresets,
       applyPreset,
     };
@@ -89,134 +86,44 @@ export default defineComponent({
           </v-icon>
         </template>
         <span>
-          The measure of the range of CT numbers that an image contains.
-          A significantly wide window displaying all the CT numbers will
-          obscure different attenuations between soft tissues.
+          TODO: explain this new widget
         </span>
       </v-tooltip>
     </v-col>
     <v-col
-      cols="7"
+      cols="9"
       style="text-align: center"
     >
-      <v-slider
-        v-mousetrap="[
-          {
-            bind: '-',
-            handler: () => updateWindow(currentWindowWidth - 5, currentWindowLevel)
-          },
-          {
-            bind: '=',
-            handler: () => updateWindow(currentWindowWidth + 5, currentWindowLevel)
-          }
-        ]"
-        :value="currentWindowWidth"
+      <v-range-slider
+        v-model="currentRange"
         :max="widthMax"
         :min="widthMin"
         class="align-center"
-        hide-details
-        :disabled="windowLocked"
-        @input="(width) => updateWindow(width, currentWindowLevel)"
+        height="5"
       >
         <template #prepend>
-          {{ widthMin }}
+          <v-text-field
+            :value="currentRange[0]"
+            class="mt-0 pt-0"
+            hide-details
+            single-line
+            type="number"
+            style="width: 60px"
+            @change="$set(currentRange, 0, $event)"
+          />
         </template>
         <template #append>
-          <div class="pr-5 pt-2">
-            {{ widthMax }}
-          </div>
+          <v-text-field
+            :value="currentRange[1]"
+            class="mt-0 pt-0"
+            hide-details
+            single-line
+            type="number"
+            style="width: 60px"
+            @change="$set(currentRange, 1, $event)"
+          />
         </template>
-      </v-slider>
-    </v-col>
-    <v-col cols="2">
-      <v-text-field
-        :value="currentWindowWidth"
-        class="mt-0 pt-0"
-        hide-details
-        single-line
-        type="number"
-        style="width: 80px; float: right"
-        :disabled="windowLocked"
-        @input="(width) => updateWindow(width, currentWindowLevel)"
-      />
-    </v-col>
-    <v-col
-      cols="1"
-      style="text-align: right"
-    >
-      <v-icon
-        v-if="!windowLocked"
-        @click="() => setWindowLocked(true)"
-      >
-        mdi-lock-open
-      </v-icon>
-      <v-icon
-        v-else
-        @click="() => setWindowLocked(false)"
-      >
-        mdi-lock
-      </v-icon>
-    </v-col>
-
-    <v-col cols="2">
-      Level
-      <v-tooltip bottom>
-        <template #activator="{ on, attrs }">
-          <v-icon
-            v-bind="attrs"
-            small
-            v-on="on"
-          >
-            info
-          </v-icon>
-        </template>
-        <span>
-          The midpoint of the range of the CT numbers displayed.
-          When the window level is decreased the CT image will be brighter.
-        </span>
-      </v-tooltip>
-    </v-col>
-    <v-col cols="7">
-      <v-slider
-        v-mousetrap="[
-          {
-            bind: '[',
-            handler: () => updateWindow(currentWindowWidth, currentWindowLevel - 5)
-          },
-          {
-            bind: ']',
-            handler: () => updateWindow(currentWindowWidth, currentWindowLevel + 5)
-          }
-        ]"
-        :value="currentWindowLevel"
-        :max="levelMax"
-        :min="levelMin"
-        class="align-center"
-        hide-details
-        :disabled="windowLocked"
-        @input="(level) => updateWindow(currentWindowWidth, level)"
-      >
-        <template #prepend>
-          {{ levelMin }}
-        </template>
-        <template #append>
-          <div class="pr-5 pt-2">
-            {{ levelMax }}
-          </div>
-        </template>
-      </v-slider>
-    </v-col>
-    <v-col cols="2">
-      <v-text-field
-        :value="currentWindowLevel"
-        class="mt-0 pt-0"
-        hide-details
-        single-line
-        type="number"
-        style="width: 80px; float: right"
-        :disabled="windowLocked"
-        @input="(level) => updateWindow(currentWindowWidth, level)"
-      />
+      </v-range-slider>
     </v-col>
     <v-col
       cols="1"
