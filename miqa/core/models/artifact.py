c57da29184e890c74719a5a77d2b7f7eef45ccee
Dave Mackey <dave@davemackey.net>
2022-08-29 10:07:21 -0400

    Update miqa/core/models/artifact.py
    Add related_name to artifact
    
    Co-authored-by: Zach Mullen <zach.mullen@kitware.com>

--- miqa/core/models/artifact.py
+++ miqa/core/models/artifact.py
@@ -11,7 +11,7 @@ class Artifact(models.Model):
 
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     name = models.CharField(max_length=255, blank=False)
-    group = models.ForeignKey('Group', on_delete=models.CASCADE)
+    group = models.ForeignKey('Group', on_delete=models.CASCADE, related_name='artifacts')
 
     def __str__(self):
         return self.name
