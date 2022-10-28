27cbd3c330dfa25e31522dcc577c90c3ddc032af
Anne Haley <anne.haley@kitware.com>
2022-10-28 09:44:09 -0400

    Hanging pages 3 (#625)
    * Hanging pages 2 (#624)
    
    * Undo previous fix
    
    * Only render ExperimentsView when currentProject is defined
    
    * Move v-if

--- web_client/src/components/ExperimentsView.vue
+++ web_client/src/components/ExperimentsView.vue
@@ -189,7 +189,10 @@ export default {
 </script>
 
 <template>
-  <v-card class="flex-card">
+  <v-card
+    v-if="currentProject"
+    class="flex-card"
+  >
     <div
       v-if="currentProject"
       class="d-flex"
