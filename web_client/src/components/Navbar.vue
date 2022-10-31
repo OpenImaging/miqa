e83b65824d590ed5cec780c7a29d05de519889ce
Anne Haley <anne.haley@kitware.com>
2022-10-31 10:39:51 -0400

    Sentry error 3562547151

--- web_client/src/components/Navbar.vue
+++ web_client/src/components/Navbar.vue
@@ -72,7 +72,11 @@ export default defineComponent({
         <template #activator="{ on }">
           <span v-on="on">MIQA</span>
         </template>
-        <span>{{ MIQAConfig.version.length > 0 ? MIQAConfig.version : "Demo Instance" }}</span>
+        <span>{{
+          MIQAConfig.version && MIQAConfig.version.length > 0
+            ? MIQAConfig.version
+            : "Demo Instance"
+        }}</span>
       </v-tooltip>
     </v-toolbar-title>
     <v-tabs
