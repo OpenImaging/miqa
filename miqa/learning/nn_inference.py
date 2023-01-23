3153b6f47e93fcb6c4292e8ffc2371d32fba3ebf
Anne Haley <anne.haley@kitware.com>
2023-01-23 12:37:46 -0500

    Use underscore in python files

--- miqa/learning/nn_inference.py
+++ miqa/learning/nn_inference.py
@@ -4,7 +4,7 @@ import math
 import itk
 import monai
 import numpy as np
-from scikit-learn.metrics import classification_report, confusion_matrix, mean_squared_error, r2_score
+from scikit_learn.metrics import classification_report, confusion_matrix, mean_squared_error, r2_score
 import torch
 from torch.utils.data import DataLoader
 import torchio
