320edf0c66ad7b6c0a3edbb6d51f2ab7311534d8
Dave Mackey <dave@davemackey.net>
2023-04-03 11:57:01 -0400

    Reorging types roughly by alphabetical order (User/Email at beginning, MIQAStore at end) to allow easier navigation between types

--- web_client/src/types.ts
+++ web_client/src/types.ts
@@ -3,6 +3,12 @@
 /* eslint no-shadow: "off" */
 import type { WorkerPool } from 'itk/WorkerPool';
 
+interface ResponseData {
+  detail: string,
+  errors: string[],
+  warnings: string[],
+}
+
 interface User {
   id: number,
   username: string,
@@ -10,10 +16,30 @@ interface User {
   is_superuser: boolean
 }
 
-interface ResponseData {
-  detail: string,
-  errors: string[],
-  warnings: string[],
+interface Email {
+  to: string[],
+  cc: string[],
+  bcc: string[],
+  subject: string,
+  body: string,
+  screenshots: any[],
+}
+
+interface Experiment {
+  id: string,
+  name: string,
+  lock_owner: { // TODO: Can remove?
+    id: number,
+    username: string,
+  },
+  lockOwner: {
+    id: number,
+    username: string,
+  },
+  // eslint-disable-next-line no-use-before-define
+  scans?: Scan[],
+  project: string,
+  note: string,
 }
 
 interface Frame {
@@ -23,6 +49,59 @@ interface Frame {
   extension: string,
   experiment?: string,
   frame_evaluation: string,
+}
+
+interface MIQAConfig {
+  version: string;
+}
+
+interface Project {
+  id: string,
+  name: string,
+  experiments?: Experiment[],
+  // eslint-disable-next-line no-use-before-define
+  settings: ProjectSettings,
+  status: {
+    total_scans: number,
+    total_complete: number,
+  }
+  creator: string;
+}
+
+interface ProjectSettings {
+  import_path: string,
+  export_path: string,
+  anatomy_orientation?: string,
+  permissions?: object,
+}
+
+interface ProjectTaskOverview {
+  project_id: string,
+  total_experiments: number,
+  total_scans: number,
+  my_project_role: string,
+  scan_states: {
+    string: string,
+  },
+}
+
+interface Scan {
+  id: string,
+  name: string,
+  scan_id: string,
+  scan_type: string,
+  experiment: string,
+  // eslint-disable-next-line no-use-before-define
+  decisions: ScanDecision[],
+  frames: Frame[],
+  subject_id: string, // TODO: Can remove?
+  subjectID: string,
+  session_id: string, // TODO: Can remove?
+  sessionID: string,
+  scan_link: string, // TODO: Can remove?
+  link: string,
+  notes: string,
+  cumulativeRange?: number,
 }
 
 interface ScanDecision {
@@ -42,86 +121,10 @@ interface ScanDecision {
   }
 }
 
-interface Scan {
-  id: string,
-  name: string,
-  scan_id: string,
-  scan_type: string,
-  experiment: string,
-  decisions: ScanDecision[],
-  frames: Frame[],
-  subject_id: string, // TODO: Can remove?
-  subjectID: string,
-  session_id: string, // TODO: Can remove?
-  sessionID: string,
-  scan_link: string, // TODO: Can remove?
-  link: string,
-  notes: string,
-  cumulativeRange?: number,
-}
-
-interface Experiment {
-  id: string,
-  name: string,
-  lock_owner: { // TODO: Can remove?
-    id: number,
-    username: string,
-  },
-  lockOwner: {
-    id: number,
-    username: string,
-  },
-  scans?: Scan[],
-  project: string,
-  note: string,
-}
-
-interface ProjectSettings {
-  import_path: string,
-  export_path: string,
-  anatomy_orientation?: string,
-  permissions?: object,
-}
-
 enum ScanState {
   unreviewed = '#1460A3',
   needs_tier_2_review = '#6DB1ED',
   complete = '#00C853',
-}
-
-interface ProjectTaskOverview {
-  project_id: string,
-  total_experiments: number,
-  total_scans: number,
-  my_project_role: string,
-  scan_states: {
-    string: string,
-  },
-}
-
-interface Project {
-  id: string,
-  name: string,
-  experiments?: Experiment[],
-  settings: ProjectSettings,
-  status: {
-    total_scans: number,
-    total_complete: number,
-  }
-  creator: string;
-}
-
-interface Email {
-  to: string[],
-  cc: string[],
-  bcc: string[],
-  subject: string,
-  body: string,
-  screenshots: any[],
-}
-
-interface MIQAConfig {
-  version: string;
 }
 
 interface WindowLock {
